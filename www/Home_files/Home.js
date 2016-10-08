e7a8734b09474b24b3d1941fd566e6413018d224
ADRIAN GROPPER <agropper@healthurl.com>
2016-10-07 21:08:52 -0400

    remove

--- www/Home_files/Home.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20161007
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Home_files/HomeMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');detectBrowser();fixAllIEPNGs('Media/transparent.gif');Widget.onload();fixupAllIEPNGBGs();performPostEffectsFixups()}
-function onPageUnload()
-{Widget.onunload();}
